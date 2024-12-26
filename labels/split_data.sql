33cd5f7202d73392f4197d5ac16cb04d5aa3ef96
Hugh Cameron <hescameron@gmail.com>
2024-12-26 19:05:23 +1000

    Export to Dataset

--- labels/split_data.sql
+++ labels/split_data.sql
@@ -1,44 +1,130 @@
-
-CREATE    TABLE type_domain AS
-SELECT    *,
-          ROW_NUMBER() OVER (
-          PARTITION BY label
-          ) AS id
-FROM      read_json (
-          'labels/learning_data/type_domain.ndjson',
-          FORMAT = 'newline_delimited',
-          COLUMNS = {"label": "string", "provider": "string", "method": "string", "locale": "string", "value": "string"}
-          );
-
-
-COPY      (
-SELECT    label,
-          provider,
-          method,
-          locale,
-          value
-FROM      type_domain
-WHERE     id % 5 != 1
-) TO 'labels/learning_data/train.parquet'
-WITH      (FORMAT 'parquet');
-
-
-COPY      (
-SELECT    label,
-          provider,
-          method,
-          locale,
-          value
-FROM      type_domain
-WHERE     id % 5 = 1
-) TO 'labels/learning_data/test.parquet'
-WITH      (FORMAT 'parquet');
-
-
--- Export to SQLite
-
 INSTALL sqlite;
 LOAD sqlite;
 
-ATTACH 'database.sqlite' AS fifa (TYPE sqlite);
-USE fifa;
+
+CREATE TABLE type_domain AS
+SELECT
+    *,
+    ROW_NUMBER() OVER (
+        PARTITION BY
+            class
+    ) AS split,
+    DENSE_RANK() OVER (
+        ORDER BY
+            class
+    ) - 1 AS label
+FROM
+read_json (
+    'labels/learning_data/type_domain.ndjson',
+    FORMAT = 'newline_delimited',
+    COLUMNS = {"class": "string", "provider": "string", "method": "string", "locale": "string", "text": "string"});
+
+-- Attach the SQLite database
+ATTACH '/Users/hugh/.cache/burn-dataset/hughcameronfinetype_01.db' AS finetype (TYPE SQLITE);
+
+CREATE TABLE train AS
+SELECT
+    ROW_NUMBER() OVER () AS row_id,
+    label,
+    class,
+    provider,
+    method,
+    locale,
+    text
+FROM
+    type_domain
+    WHERE split % 5 != 1
+    ;
+
+CREATE TABLE test AS
+SELECT
+    ROW_NUMBER() OVER () AS row_id,
+    label,
+    class,
+    provider,
+    method,
+    locale,
+    text
+FROM
+    type_domain
+    WHERE split % 5 = 1
+    ;
+
+
+CREATE
+OR REPLACE TABLE finetype.test (
+    row_id INTEGER,
+    label INTEGER,
+    class VARCHAR,
+    provider VARCHAR,
+    method VARCHAR,
+    locale VARCHAR,
+    text VARCHAR
+);
+
+-- Insert data from the DuckDB table into the SQLite table
+INSERT INTO
+    finetype.test
+SELECT
+    *
+FROM
+    test;
+
+
+CREATE
+    OR REPLACE TABLE finetype.train (
+        row_id INTEGER,
+        label INTEGER,
+        class VARCHAR,
+        provider VARCHAR,
+        method VARCHAR,
+        locale VARCHAR,
+        text VARCHAR
+    );
+
+
+INSERT INTO
+    finetype.train
+SELECT
+    *
+FROM
+    train;
+
+
+CREATE
+    OR REPLACE TABLE finetype.labels (
+        label INTEGER,
+        class VARCHAR
+    );
+
+
+INSERT INTO
+    finetype.labels
+SELECT
+distinct
+    label,
+    class
+    from type_domain;
+
+COPY (
+    SELECT
+        *
+    FROM
+        train
+) TO 'labels/learning_data/train.parquet'
+WITH
+    (FORMAT 'parquet');
+
+COPY (
+    SELECT
+        *
+    FROM
+        test
+) TO 'labels/learning_data/test.parquet'
+WITH
+    (FORMAT 'parquet');
+
+
+COPY (select distinct label, class from type_domain order by label)
+TO 'labels/learning_data/labels.parquet' WITH (FORMAT 'parquet')
+;
