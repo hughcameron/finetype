baa9f5203e13d9eefb5b566afa934b5bb6432120
Hugh Cameron <hescameron@gmail.com>
2024-12-26 19:04:55 +1000

    Generate at Priority 5

--- labels/build.sh
+++ labels/build.sh
@@ -1,5 +1,7 @@
 va eda
 
+python generate_data.py --priority 5
+
 huggingface-cli upload hughcameron/FineType_Labels_01 labels/learning_data/test.parquet --repo-type dataset
 huggingface-cli upload hughcameron/FineType_Labels_01 labels/learning_data/train.parquet --repo-type dataset
 
