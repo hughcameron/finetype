d6008b58ab6eb2f2b3f7f4beaf44dadf64ba67a5
Hugh Cameron <hescameron@gmail.com>
2024-12-28 16:33:50 +1000

    Support different input types

--- classifier/src/bin/infer.rs
+++ classifier/src/bin/infer.rs
@@ -1,6 +1,9 @@
 use burn::tensor::backend::AutodiffBackend;
+use clap::{Arg, Command};
 use classifier::data::FineTypeDataset;
-use std::io::{self, BufRead};
+use serde_json::Value;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 #[cfg(not(feature = "f16"))]
 #[allow(dead_code)]
@@ -8,22 +11,52 @@ type ElemType = f32;
 #[cfg(feature = "f16")]
 type ElemType = burn::tensor::f16;
 
-pub fn launch<B: AutodiffBackend>(device: B::Device) {
-    let stdin = io::stdin();
-    let handle = stdin.lock();
+pub fn launch<B: AutodiffBackend>(device: B::Device, input: Option<String>, file: Option<String>) {
+    let inputs: Vec<String> = if let Some(input) = input {
+        vec![input]
+    } else if let Some(file) = file {
+        let file = File::open(file).expect("Unable to open file");
+        let reader = BufReader::new(file);
+        reader
+            .lines()
+            .map(|line| line.expect("Unable to read line"))
+            .collect()
+    } else {
+        let stdin = io::stdin();
+        let handle = stdin.lock();
+        handle
+            .lines()
+            .map(|line| line.expect("Unable to read line"))
+            .collect()
+    };
 
-    for line in handle.lines() {
-        if let Ok(input) = line {
-            if input.trim().is_empty() {
-                continue;
+    for input in inputs {
+        if input.trim().is_empty() {
+            continue;
+        }
+
+        let predictions: Vec<String> = if let Ok(json_value) = serde_json::from_str::<Value>(&input)
+        {
+            match json_value {
+                Value::String(s) => vec![s],
+                Value::Array(arr) => arr
+                    .into_iter()
+                    .filter_map(|v| v.as_str().map(|s| s.to_string()))
+                    .collect(),
+                _ => vec![],
             }
-            let prediction = classifier::inference::infer_single::<B, FineTypeDataset>(
+        } else {
+            vec![input]
+        };
+
+        for prediction in predictions {
+            let result = classifier::inference::infer_single::<B, FineTypeDataset>(
                 device.clone(),
                 "/tmp/classifier-finetype",
-                input.trim().to_string(),
+                prediction.trim().to_string(),
             );
 
-            println!("{}", prediction);
+            println!("{}", result);
         }
     }
 }
@@ -42,8 +75,8 @@ mod ndarray {
 
     use crate::launch;
 
-    pub fn run() {
-        launch::<Autodiff<NdArray<f32>>>(NdArrayDevice::Cpu);
+    pub fn run(input: Option<String>, file: Option<String>) {
+        launch::<Autodiff<NdArray<f32>>>(NdArrayDevice::Cpu, input, file);
     }
 }
 
@@ -56,13 +89,13 @@ mod tch_gpu {
 
     use crate::launch;
 
-    pub fn run() {
+    pub fn run(input: Option<String>, file: Option<String>) {
         #[cfg(not(target_os = "macos"))]
         let device = LibTorchDevice::Cuda(0);
         #[cfg(target_os = "macos")]
         let device = LibTorchDevice::Mps;
 
-        launch::<Autodiff<LibTorch<f32>>>(device);
+        launch::<Autodiff<LibTorch<f32>>>(device, input, file);
     }
 }
 
@@ -75,8 +108,8 @@ mod tch_cpu {
 
     use crate::launch;
 
-    pub fn run() {
-        launch::<Autodiff<LibTorch<f32>>>(LibTorchDevice::Cpu);
+    pub fn run(input: Option<String>, file: Option<String>) {
+        launch::<Autodiff<LibTorch<f32>>>(LibTorchDevice::Cpu, input, file);
     }
 }
 
@@ -89,23 +122,48 @@ mod wgpu {
 
     use crate::launch;
 
-    pub fn run() {
-        launch::<Autodiff<Wgpu<f32, i32>>>(WgpuDevice::default());
+    pub fn run(input: Option<String>, file: Option<String>) {
+        launch::<Autodiff<Wgpu<f32, i32>>>(WgpuDevice::default(), input, file);
     }
 }
 
 fn main() {
+    let matches = Command::new("FineType")
+        .version("1.0")
+        .author("Hugh Cameron (https://github.com/hughcameron)")
+        .about("Classifies text")
+        .arg(
+            Arg::new("input")
+                .short('i')
+                .long("input")
+                .value_name("INPUT")
+                .help("Single JSON string or JSON array of strings")
+                .value_parser(clap::value_parser!(String)),
+        )
+        .arg(
+            Arg::new("file")
+                .short('f')
+                .long("file")
+                .value_name("FILE")
+                .help("File containing text input")
+                .value_parser(clap::value_parser!(String)),
+        )
+        .get_matches();
+
+    let input = matches.get_one::<String>("input").cloned();
+    let file = matches.get_one::<String>("file").cloned();
+
     #[cfg(any(
         feature = "ndarray",
         feature = "ndarray-blas-netlib",
         feature = "ndarray-blas-openblas",
         feature = "ndarray-blas-accelerate",
     ))]
-    ndarray::run();
+    ndarray::run(input, file);
     #[cfg(feature = "tch-gpu")]
-    tch_gpu::run();
+    tch_gpu::run(input, file);
     #[cfg(feature = "tch-cpu")]
-    tch_cpu::run();
+    tch_cpu::run(input, file);
     #[cfg(feature = "wgpu")]
-    wgpu::run();
+    wgpu::run(input, file);
 }
--- labels/build.sh
+++ labels/build.sh
@@ -1,10 +1,32 @@
+# Load python environment:
 va eda
 
+# Generate training data:
 python generate_data.py --priority 5
 
+# Generate validation data:
 python generate_data.py --texts 2 --output "learning_data/validation.ndjson"
 
+# Split and release the data:
+duckdb < split_data.sql
+
+# Upload the data:
 huggingface-cli upload hughcameron/FineType_Labels_01 labels/learning_data/test.parquet --repo-type dataset
 huggingface-cli upload hughcameron/FineType_Labels_01 labels/learning_data/train.parquet --repo-type dataset
 
-duckdb < split_data.sql
+# Training the model:
+cargo run --bin train --release --features wgpu
+
+# Inference binary release:
+cargo build --release --bin infer --features wgpu
+
+# STDIN inference:
+echo "bc89:60a9:23b8:c1e9:3924:56de:3eb1:3b90" | ./classifier/target/release/infer
+
+# Inference from input:
+./classifier/target/release/infer -i "bc89:60a9:23b8:c1e9:3924:56de:3eb1:3b90"
+./classifier/target/release/infer -i '["bc89:60a9:23b8:c1e9:3924:56de:3eb1:3b90", "00:16:3e:1c:0c:8c"]'
+
+# Inference from file:
+echo '["bc89:60a9:23b8:c1e9:3924:56de:3eb1:3b90", "00:16:3e:1c:0c:8c"]' > data.json
+./classifier/target/release/infer -f data.json
