51ddcb333ffef026e84eb9461e89cd245e165649
Hugh Cameron <hescameron@gmail.com>
2024-12-28 14:27:08 +1000

    Infer from STDIN

--- classifier/src/bin/infer.rs
+++ classifier/src/bin/infer.rs
@@ -1,6 +1,6 @@
-use classifier::data::FineTypeDataset;
-
 use burn::tensor::backend::AutodiffBackend;
+use classifier::data::FineTypeDataset;
+use std::io::{self, BufRead};
 
 #[cfg(not(feature = "f16"))]
 #[allow(dead_code)]
@@ -9,45 +9,23 @@ type ElemType = f32;
 type ElemType = burn::tensor::f16;
 
 pub fn launch<B: AutodiffBackend>(device: B::Device) {
-    classifier::inference::infer::<B, FineTypeDataset>(
-        device,
-        "/tmp/classifier-finetype",
-        // Samples from the test dataset, but you are free to test with your own text.
-        vec![
-            "9f143a4f-1be7-4d88-b9c4-0098edf36e82".to_string(),
-            "bf23468d-efab-42a4-b0a3-be9161709ad7".to_string(),
-            "1706048114".to_string(),
-            "1732431937".to_string(),
-            "America/Argentina/Catamarca".to_string(),
-            "Asia/Jakarta".to_string(),
-            "AS2746317214".to_string(),
-            "AS478163328".to_string(),
-            "163.177.121.157".to_string(),
-            "70.104.82.87".to_string(),
-            "163.177.121.157:1640".to_string(),
-            "189.214.64.251:16050".to_string(),
-            "bdd6:40fb:667:1ad1:1c80:317f:a3b1:799d".to_string(),
-            "bc89:60a9:23b8:c1e9:3924:56de:3eb1:3b90".to_string(),
-            "00:16:3e:1c:0c:8c".to_string(),
-            "00:16:3e:3e:72:47".to_string(),
-            "8.8.8.8".to_string(),
-            "1.1.1.1".to_string(),
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.78.2 (KHTML, like Gecko) Version/7.0.6 Safari/537.78.2".to_string(),
-            "Mozilla/5.0 (Linux; Android 11; Pixel 5 Build/RQ3A.210805.001.A1; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/92.0.4515.159 Mobile Safari/537.36".to_string(),
-            "1brnTP3fAbnFbmOHnKYaXRvj7uff0LYTH8".to_string(),
-            "3esM2wleOV911xCZkwPRQeNHpCq5QnuVdY".to_string(),
-            "3404 332181 96006".to_string(),
-            "4223 3890 8386 3795".to_string(),
-            "0x46685257bdd640fb06671ad11c80317fa3b1799d".to_string(),
-            "0x1a3d1fa7bc8960a923b8c1e9392456de3eb13b90".to_string(),
-            "GTCTGGATCT".to_string(),
-            "TGTTGGTGAT".to_string(),
-            "GUCUGGAUCU".to_string(),
-            "UGUUGGUGAU".to_string(),
-            "#390062".to_string(),
-            "#0cce35".to_string()
-        ],
-    );
+    let stdin = io::stdin();
+    let handle = stdin.lock();
+
+    for line in handle.lines() {
+        if let Ok(input) = line {
+            if input.trim().is_empty() {
+                continue;
+            }
+            let prediction = classifier::inference::infer_single::<B, FineTypeDataset>(
+                device.clone(),
+                "/tmp/classifier-finetype",
+                input.trim().to_string(),
+            );
+
+            println!("{}", prediction);
+        }
+    }
 }
 
 #[cfg(any(
@@ -62,10 +40,10 @@ mod ndarray {
         Autodiff,
     };
 
-    use crate::{launch, ElemType};
+    use crate::launch;
 
     pub fn run() {
-        launch::<Autodiff<NdArray<ElemType>>>(NdArrayDevice::Cpu);
+        launch::<Autodiff<NdArray<f32>>>(NdArrayDevice::Cpu);
     }
 }
 
@@ -76,7 +54,7 @@ mod tch_gpu {
         Autodiff,
     };
 
-    use crate::{launch, ElemType};
+    use crate::launch;
 
     pub fn run() {
         #[cfg(not(target_os = "macos"))]
@@ -84,7 +62,7 @@ mod tch_gpu {
         #[cfg(target_os = "macos")]
         let device = LibTorchDevice::Mps;
 
-        launch::<Autodiff<LibTorch<ElemType>>>(device);
+        launch::<Autodiff<LibTorch<f32>>>(device);
     }
 }
 
@@ -95,10 +73,10 @@ mod tch_cpu {
         Autodiff,
     };
 
-    use crate::{launch, ElemType};
+    use crate::launch;
 
     pub fn run() {
-        launch::<Autodiff<LibTorch<ElemType>>>(LibTorchDevice::Cpu);
+        launch::<Autodiff<LibTorch<f32>>>(LibTorchDevice::Cpu);
     }
 }
 
@@ -109,10 +87,10 @@ mod wgpu {
         Autodiff,
     };
 
-    use crate::{launch, ElemType};
+    use crate::launch;
 
     pub fn run() {
-        launch::<Autodiff<Wgpu<ElemType, i32>>>(WgpuDevice::default());
+        launch::<Autodiff<Wgpu<f32, i32>>>(WgpuDevice::default());
     }
 }
 
--- classifier/src/inference.rs
+++ classifier/src/inference.rs
@@ -16,12 +16,11 @@ use burn::{
 };
 use std::sync::Arc;
 
-// Define inference function
-pub fn infer<B: Backend, D: TextClassificationDataset + 'static>(
-    device: B::Device, // Device on which to perform computation (e.g., CPU or CUDA device)
-    artifact_dir: &str, // Directory containing model and config files
-    samples: Vec<String>, // Text samples for inference
-) {
+pub fn infer_single<B: Backend, D: TextClassificationDataset + 'static>(
+    device: B::Device,
+    artifact_dir: &str,
+    sample: String,
+) -> String {
     // Load experiment configuration
     let config = ExperimentConfig::load(format!("{artifact_dir}/config.json").as_str())
         .expect("Config file present");
@@ -40,13 +39,11 @@ pub fn infer<B: Backend, D: TextClassificationDataset + 'static>(
     ));
 
     // Load pre-trained model weights
-    println!("Loading weights ...");
     let record = CompactRecorder::new()
         .load(format!("{artifact_dir}/model").into(), &device)
         .expect("Trained model weights");
 
     // Create model using loaded weights
-    println!("Creating model ...");
     let model = TextClassificationModelConfig::new(
         config.transformer,
         n_classes,
@@ -54,25 +51,13 @@ pub fn infer<B: Backend, D: TextClassificationDataset + 'static>(
         config.max_seq_length,
     )
     .init(&device)
-    .load_record(record); // Initialize model with loaded weights
+    .load_record(record);
 
-    // Run inference on the given text samples
-    println!("Running inference ...");
-    let item = batcher.batch(samples.clone()); // Batch samples using the batcher
-    let predictions = model.infer(item); // Get model predictions
+    // Run inference on the given text sample
+    let item = batcher.batch(vec![sample.clone()]); // Batch the single sample
+    let predictions = model.infer(item);
 
-    // Print out predictions for each sample
-    for (i, text) in samples.into_iter().enumerate() {
-        #[allow(clippy::single_range_in_vec_init)]
-        let prediction = predictions.clone().slice([i..i + 1]); // Get prediction for current sample
-        let logits = prediction.to_data(); // Convert prediction tensor to data
-        let class_index = prediction.argmax(1).squeeze::<1>(1).into_scalar(); // Get class index with the highest value
-        let class = D::class_name(class_index.elem::<i32>() as usize); // Get class name
-
-        // Print sample text, predicted logits and predicted class
-        println!(
-            "\n=== Item {i} ===\n- Text: {text}\n- Logits: {logits}\n- Prediction: \
-             {class}\n================"
-        );
-    }
+    let prediction = predictions.slice([0..1]); // Get prediction for the sample
+    let class_index = prediction.argmax(1).squeeze::<1>(1).into_scalar(); // Get class index
+    D::class_name(class_index.elem::<i32>() as usize) // Return the class name
 }
